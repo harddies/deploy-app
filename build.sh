0f41e010264be8899534c83e08f9685ae2098021
harddies827@126.com <harddies827@126.com>
2023-04-14 17:20:36 +0800

    v1

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo 'start building'
+echo -n '请输入分支名称：'
+read branch_name
+echo -n '项目名称：'
+read project_name
+echo -n '服务名称：'
+read service_name
+
+cd /root/deploy-app/kratos-v2-demo
+git pull
+git checkout ${branch_name}
+version=`git log --pretty=oneline -n 1| awk '{print $1}'`
+cd /root/deploy-app
+DOCKER_BUILDKIT=1 docker build --build-arg project_name=${project_name} \
+--build-arg service_name=${service_name} \
+-t registry.my-cluster.co:5000/$project_name-$service_name \
+-f ./build-go-app --output out/$project_name-$service_name .
+
+touch out/$project_name-$service_name/version
+echo $version > out/$project_name-$service_name/version
+
+echo 'finished building'
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo 'start building'
+echo -n '项目名称：'
+read project_name
+echo -n '服务名称：'
+read service_name
+
+cd /root/deploy-app
+version=`cat out/$project_name-$service_name/version`
+docker build --build-arg project_name=${project_name} \
+--build-arg service_name=${service_name} \
+--label version=$version \
+-t registry.my-cluster.co:5000/$project_name-$service_name \
+-f ./run-go-app .
+
+docker push registry.my-cluster.co:5000/$project_name-$service_name
+
+echo 'finished building'
