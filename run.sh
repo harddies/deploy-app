3dfb9ce499139992c8047a3b9932a7505eb79f06
harddies827@126.com <harddies827@126.com>
2023-04-17 19:48:26 +0800

    v1

--- run.sh
+++ run.sh
@@ -16,6 +16,6 @@ docker build --build-arg project_name=${project_name} \
 -t 192.168.1.128:5000/$project_name-$service_name \
 -f ./run-go-app .
 
-docker push registry.my-cluster.co:5000/$project_name-$service_name
+docker push 192.168.1.128:5000/$project_name-$service_name
 
 echo 'finished building'
