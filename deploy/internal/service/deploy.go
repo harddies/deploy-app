de6508828e18d518036a8834e092f7121b9ecd68
harddies827@126.com <harddies827@126.com>
2023-04-22 20:17:17 +0800

    v1.1

--- deploy/internal/service/deploy.go
+++ deploy/internal/service/deploy.go
@@ -11,6 +11,7 @@ import (
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	applyAppsV1 "k8s.io/client-go/applyconfigurations/apps/v1"
+	applyCoreV1 "k8s.io/client-go/applyconfigurations/core/v1"
 )
 
 func (s *Service) ApplyDeployment(d string) {
@@ -27,13 +28,24 @@ func (s *Service) ApplyDeployment(d string) {
 	}
 	b := buf.Bytes()
 
+	ctx := context.Background()
 	dac := new(applyAppsV1.DeploymentApplyConfiguration)
 	if err = yaml.Unmarshal(b, dac); err != nil {
-		log.Fatalf("yaml.Unmarshal %+v", err)
+		log.Fatalf("yaml.Unmarshal deployment %+v", err)
 	}
 	if _, err = s.k8sClient.AppsV1().Deployments(model.K8sNamespaceApp).
-		Apply(context.Background(), dac, metaV1.ApplyOptions{FieldManager: model.K8sFieldManager}); err != nil {
-		log.Fatalf("k8s apply %+v", err)
+		Apply(ctx, dac, metaV1.ApplyOptions{FieldManager: model.K8sFieldManager}); err != nil {
+		log.Fatalf("k8s apply deployment %+v", err)
 	}
+
+	svc := new(applyCoreV1.ServiceApplyConfiguration)
+	if err = yaml.Unmarshal(b, svc); err != nil {
+		log.Fatalf("yaml.Unmarshal svc %+v", err)
+	}
+	if _, err = s.k8sClient.CoreV1().Services(model.K8sNamespaceApp).
+		Apply(ctx, svc, metaV1.ApplyOptions{FieldManager: model.K8sFieldManager}); err != nil {
+		log.Fatalf("k8s apply svc %+v", err)
+	}
+
 	log.Println("apply deployment successfully")
 }
